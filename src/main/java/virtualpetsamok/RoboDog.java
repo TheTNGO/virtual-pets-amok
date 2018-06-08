2bef736f44227cd62366c8e4d699f0a896055b33
Thomas Ngo <thoas015@gmail.com>
2018-06-08 00:48:32 -0400

    RoboDog Complete

--- src/main/java/virtualpetsamok/RoboDog.java
+++ src/main/java/virtualpetsamok/RoboDog.java
@@ -2,4 +2,12 @@ package virtualpetsamok;
 
 public class RoboDog extends RoboPet {
 
+	public void inputWalk() {
+		this.setStatHappiness(this.getStatHappiness() + 5);
+	}
+
+	public void tick() {
+		super.tick();
+	}
+
 }
--- src/test/java/virtualpetsamok/RoboDogTest.java
+++ src/test/java/virtualpetsamok/RoboDogTest.java
@@ -1,6 +1,7 @@
 package virtualpetsamok;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
@@ -13,6 +14,32 @@ public class RoboDogTest {
 	@Before
 	public void setUp() {
 		testRoboDog = new RoboDog();
+		
+		
+	}
+	
+	@Test
+	public void shouldHaveOilLevel() {
+		int oil = testRoboDog.getStatOil();
+		assertEquals(15, oil);
+
+	}
+
+	@Test
+	public void shouldSubtractHealthByOneWhenOilIsAt0OnTick() {
+		int healthBefore = testRoboDog.getStatHealth();
+		testRoboDog.setStatOil(0);
+		testRoboDog.tick();
+		int healthAfter = testRoboDog.getStatHealth();
+		assertThat(healthAfter, is(healthBefore - 1));
+	}
+
+	public void shouldSubtractHappinessByTwoWhenOilIsAt0OnTick() {
+		int happinessBefore = testRoboDog.getStatHealth();
+		testRoboDog.setStatOil(0);
+		testRoboDog.tick();
+		int happinessAfter = testRoboDog.getStatHealth();
+		assertThat(happinessAfter, is(happinessBefore - 2));
 	}
 	
 	@Test
